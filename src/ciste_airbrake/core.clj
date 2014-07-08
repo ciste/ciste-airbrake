95a402eec1ea6f9a289da9a8d6c03b6c5cfd78f3
Daniel E. Renfer <duck@kronkltd.net>
2014-07-08 18:06:21 -0400

    core functions

--- src/ciste_airbrake/core.clj
+++ src/ciste_airbrake/core.clj
@@ -1,5 +1,26 @@
 (ns ciste-airbrake.core)
 
+(defn send-airbrake
+  [ex options]
+  (airbrake/notify
+   (config :airbrake :key)
+   (name @*environment*)
+   "/" ex options))
+
+(defn errors
+  [ex]
+        (when (config :airbrake :enabled)
+        (let [options {:url "foo"
+                       :params (into {} (map (fn [[k v]] {k (pr-str v)})
+                                             (:environment data)))}]
+          (try
+            (send-airbrake ex options)
+            (catch Exception ex))))
+
+  )
+
+
+
 (defn foo
   "I don't do a whole lot."
   [x]
